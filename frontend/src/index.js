2d6e32ee5e9683f3d8f3385246d8eb85e34b8c15
mangosmoothie <mangosmoothie@users.noreply.github.com>
2017-09-24 23:39:42 -0500

    doing thunk now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers'
-import axios from 'axios'
-import { addTag } from './picturedisplay/actions/tags'
+import { fetchTags } from './picturedisplay/actions/tags'
 
-let store = createStore(reducers)
+const loggerMiddleware = createLogger()
 
-axios.get('/api/all-tags')
-  .then( ({data}) => {
-    data['tags'].map(t => store.dispatch(addTag(t)))
-  })
-  .catch( (err) => console.error(err) );
+let store = createStore(
+  reducers,
+  applyMiddleware(thunk, loggerMiddleware)
+)
+
+store.dispatch(fetchTags())
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/picturedisplay/actions/tags.js
+++ frontend/src/picturedisplay/actions/tags.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const TOGGLE_TAG = 'TOGGLE_TAG'
 
 export function toggleTagField(tagId, field){
@@ -13,3 +15,21 @@ export const ADD_TAG = 'ADD_TAG'
 export function addTag(tag){
   return { type: ADD_TAG, tag: tag }
 }
+
+export const REQUEST_TAGS = 'REQUEST_TAGS'
+
+function requestTags(){
+  return {
+    type: REQUEST_TAGS
+  }
+}
+
+export function fetchTags(){
+  return dispatch => {
+    dispatch(requestTags())
+    return axios.get('/api/all-tags')
+      .then( ({data}) => data['tags'].map(t => dispatch(addTag(t))),
+             (err) => console.error(err) )
+  }
+}
+
